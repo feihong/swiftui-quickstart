a50eca2ae94a37aef3fa70a0d74e11327317d389
Feihong Hsu <feihong.hsu@gmail.com>
2019-10-26 16:41:39 -0500

    Initial commit

---
+++ HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  HelloSwiftUI
+//
+//  Created by Feihong Hsu on 10/26/19.
+//  Copyright © 2019 Feihong Hsu. All rights reserved.
+//
+
+import SwiftUI
+
+func randomHanzi() -> String {
+    let code = Int.random(in: 0x4e00 ... 0x9fff)
+    if let scalar = UnicodeScalar(code) {
+      return String(scalar)
+    } else {
+        return "N/A"
+    }
+}
+
+struct ContentView: View {
+    @State var text = "你好世界！"
+    
+    var body: some View {
+        VStack {
+            Text("\(text)").font(.largeTitle)
+            Divider()
+            Button(action: {
+                self.text = String(randomHanzi())
+                print("what")
+            }) {
+                Text("Click me now!")
+                    .font(.title)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(20)
+                    .foregroundColor(.white)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
